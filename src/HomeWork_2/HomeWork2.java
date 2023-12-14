70727a040e9a63cc885133c198e02bdf2be2103c
Kiryl Kunavich <kirill99hockey@gmail.com>
2023-12-15 00:03:21 +0300

    1.2

--- src/HomeWork_2/HomeWork2.java
+++ src/HomeWork_2/HomeWork2.java
@@ -10,10 +10,10 @@ public class HomeWork2 {
         // В обобщённом методе вам необходимо в качестве параметра T принимать различные
         // подклассы животных и выводить название животного и его цвет.
 
-        Cat cat = new Cat("Tom", "Black");
-        Dog dog = new Dog("Ben", "Grey");
-        Horse horse = new Horse("Danny", "White");
-        Parrot parrot = new Parrot("Bob", "Red-Green-White");
+        Animal cat = new Cat("Tom", "Black");
+        Animal dog = new Dog("Ben", "Grey");
+        Animal horse = new Horse("Danny", "White");
+        Animal parrot = new Parrot("Bob", "Red-Green-White");
 
         Generic<Animal> generic =new Generic<>();
         generic.animals(cat);
